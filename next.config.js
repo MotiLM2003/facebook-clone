9df11d5b04c85ca643e6b95162e3d7563b8e56e3
Moti Elmakyes <motiphone203@gmail.com>
2022-11-25 21:24:14 +0200

    :apple: initial

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: [
+			'links.papareact.com',
+			'platform-lookaside.fbsx.com',
+			'firebasestorage.googleapis.com',
+		],
+	},
+};
